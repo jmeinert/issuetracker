0048d8a2ffbe8dac45b031fd77a5f4d45040019b
Jan Meinert <jan.meinert1998@gmail.com>
2026-07-07 13:08:40 +0200

    Add project entity

---
+++ src/main/java/de/jmeinert/issuetracker/project/Project.java
@@ -0,0 +1,59 @@
+package de.jmeinert.issuetracker.project;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    protected Project() {
+    }
+
+    public Project(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void update(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.updatedAt = LocalDateTime.now();
+    }
+}
